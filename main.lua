f307f144784110cd8f9564478db1eac819edbf73
ropoko <rodrigostramantinoli@gmail.com>
2023-02-16 16:52:46 -0300

    feat: implemented rope

--- main.lua
+++ main.lua
@@ -1,9 +1,13 @@
 local world = require('world')
 local player = require('player')
 
+local rope = {}
+
 function love.load()
 	world:load()
 	player:load(world.instanceof)
+
+	rope = love.physics.newRopeJoint(player.body, world.roof.body, player.x, player.y, 500, 100, 200, true)
 end
 
 function love.update(dt)
@@ -14,4 +18,8 @@ end
 function love.draw()
 	world:draw()
 	player:draw()
+
+	love.graphics.setColor(1,0,0)
+	love.graphics.line(rope:getAnchors())
+	love.graphics.setColor(1,1,1)
 end
--- world.lua
+++ world.lua
@@ -1,5 +1,6 @@
 local World = {
 	ground = {},
+	roof = {},
 	instanceof = {}
 }
 
@@ -9,6 +10,10 @@ function World:load()
 	self.ground.body = love.physics.newBody(world, 400, 500, 'static')
 	self.ground.shape = love.physics.newRectangleShape(300, 50)
 	self.ground.fixture = love.physics.newFixture(self.ground.body, self.ground.shape)
+
+	self.roof.body = love.physics.newBody(world, 400, 100, 'static')
+	self.roof.shape = love.physics.newRectangleShape(300, 50)
+	self.roof.fixture = love.physics.newFixture(self.roof.body, self.roof.shape)
 
 	self.instanceof = world
 end
@@ -20,6 +25,8 @@ end
 function World:draw()
 	love.graphics.setColor(1,0.5,0.3)
   love.graphics.polygon('fill', self.ground.body:getWorldPoints(self.ground.shape:getPoints()))
+
+	love.graphics.polygon('fill', self.roof.body:getWorldPoints(self.roof.shape:getPoints()))
 end
 
 return World
